8d327298326845f31270aa1744c6a36eb56809ad
RWVro <105106146+RWVro@users.noreply.github.com>
2023-04-03 10:20:16 +0200

    Create DCC_Monitor_Cutout_INT_Test_FallingOnly.ino

---
+++ DCC_Monitor_Cutout_INT_Test_FallingOnly.ino
@@ -0,0 +1,65 @@
+
+//====================================== Monitor DCC Cutouts Program ==================================
+//
+//
+//=================================  Board = ESP32 Dev Module (NodeMCU ESP32) =========================
+
+#define ONBOARD_LED 2
+
+#define railComRX 16
+#define railComTX 17
+#define railComInt 15
+
+bool intStart = false;
+bool intStop = false;
+
+//===================================== ISR GPIO15 ================
+
+void IRAM_ATTR GPIO15ToLow()
+{
+  detachInterrupt(digitalPinToInterrupt(railComInt));
+  intStart = true;
+}
+
+//======================================== Setup =======================================
+
+void PrintFileName()
+{
+  Serial.println("");
+  Serial.println("");
+  Serial.println(__FILE__" ("__DATE__") "__TIME__);
+  Serial.println("");
+}
+
+void setup()
+{
+  Serial.begin(500000);
+  delay(1000);
+  PrintFileName();
+
+  pinMode(ONBOARD_LED, OUTPUT);           // For test Blue Led
+  digitalWrite(ONBOARD_LED, HIGH);
+
+  pinMode(railComInt, INPUT);             // GPIO15 input pin  int Raicom Detector
+
+  delay(1000);
+  Serial.println("Test interrupt started!");
+
+  pinMode(ONBOARD_LED, OUTPUT);           // For test   Blue Led
+  digitalWrite(ONBOARD_LED, LOW);
+
+  delay(3000);
+
+  attachInterrupt((railComInt), GPIO15ToLow, FALLING);
+}
+
+//======================================= Main Program ==============================
+
+void loop()
+{
+  if (intStart)
+  {  
+    Serial.println("FALLING");
+    attachInterrupt((railComInt), GPIO15ToLow, FALLING);
+  }
+}
